245fb4fd3d53f89143d65aaf51c312e6b18546e3
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-27 18:31:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  passengers.each do |suite, name| 
+    if suite == :suite_a && name[0] == "A" 
+      winner = name 
+    end 
+  end 
+  winner 
 end
